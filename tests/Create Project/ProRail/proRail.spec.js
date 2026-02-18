4b586a7fe8985d5ce88c751e2450d6ca0942d592
Md Asaduzzaman Noor <asaduzzaman.noor@selisegroup.com>
2026-02-18 23:47:34 +0600

    update before all

--- tests/Create Project/ProRail/proRail.spec.js
+++ tests/Create Project/ProRail/proRail.spec.js
@@ -11,18 +11,11 @@ test.beforeAll("Navigated to dashboard", async ({ browser }) => {
   const page = await context.newPage();
   const loginPage = new LoginPage(page);
 
-  try {
-    await loginPage.goto();
-    await page.locator(".MuiGrid-root").nth(1).isVisible();
-    await loginPage.verifyInitialState();
-    await loginPage.login();
-    await page.reload(); // Ensure the page is fully loaded after login
-    await loginPage.logoutVisible();
-  } catch (error) {
-    await page.screenshot({ path: "login-failure.png", fullPage: true });
-    console.error("Login test failed:", error.message);
-    throw error;
-  }
+  await loginPage.goto();
+  await page.locator(".MuiGrid-root").nth(1).isVisible();
+  await loginPage.verifyInitialState();
+  await loginPage.login();
+  await loginPage.logoutVisible();
 
   await context.storageState({ path: "state.json" });
   webContext = await browser.newContext({ storageState: "state.json" });
