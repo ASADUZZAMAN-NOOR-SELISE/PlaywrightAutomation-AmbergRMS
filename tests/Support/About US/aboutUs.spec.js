5bd37dd2f81717aa63e03630beede03d78bedb98
ASADUZZAMAN-NOOR-SELISE <asaduzzaman.noor@selisegroup.com>
2026-03-01 09:49:44 +0600

    Project tree (#30)
    * update
    
    * update before.all function
    
    * update try
    
    * Remove state.json from tracking
    
    * update
    
    * update
    
    * update before all
    
    * config file update

--- tests/Support/About US/aboutUs.spec.js
+++ tests/Support/About US/aboutUs.spec.js
@@ -11,18 +11,11 @@ test.beforeAll("Navigated to dashboard", async ({ browser }) => {
   const page = await context.newPage();
   const loginPage = new LoginPage(page);
 
-  try {
-    await loginPage.goto();
-    await page.locator(".MuiGrid-root").nth(1).isVisible();
-    await loginPage.verifyInitialState();
-    await loginPage.login();
-    await loginPage.logoutVisible();
-    console.log("Login successful, proceeding with tests.");
-  } catch (error) {
-    await page.screenshot({ path: "login-failure.png", fullPage: true });
-    console.error("Login test failed:", error.message);
-    throw error;
-  }
+  await loginPage.goto();
+  await page.locator(".MuiGrid-root").nth(1).isVisible();
+  await loginPage.verifyInitialState();
+  await loginPage.login();
+  await loginPage.logoutVisible();
 
   await context.storageState({ path: "state.json" });
   webContext = await browser.newContext({ storageState: "state.json" });
