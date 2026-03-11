856d2899ed48b088078e2c86711dbc4f03acd2ae
Samiha Hossain <samiha.hossain@selisegroup.com>
2026-03-11 15:12:13 +0600

    feat: tests added for STN template

---
+++ tests/Settings/Configure Template Manager/STN Template/stnTemplate.spec.js
@@ -0,0 +1,39 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../../../Utils/loginPage";
+import { STNTemplatePage } from "./stnTemplate.page";
+
+let webContext;
+
+test.beforeAll("Navigated to dashboard", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const loginPage = new LoginPage(page);
+
+  try {
+    await loginPage.goto();
+    await page.locator(".MuiGrid-root").nth(1).isVisible();
+    await loginPage.verifyInitialState();
+    await loginPage.login();
+    await loginPage.logoutVisible();
+    console.log("Login successful, proceeding with tests.");
+  } catch (error) {
+    await page.screenshot({ path: "login-failure.png", fullPage: true });
+    console.error("Login test failed:", error.message);
+    throw error;
+  }
+
+  await context.storageState({ path: "state.json" });
+  webContext = await browser.newContext({ storageState: "state.json" });
+});
+
+test("STN Template validation", async () => {
+  const page = await webContext.newPage();
+
+  const loginPage = new LoginPage(page);
+  const stnPage = new STNTemplatePage(page);
+
+  await loginPage.goto();
+  await stnPage.navigateCTM();
+  await stnPage.stnTemplate();
+  console.log("STN Template tests passed successfully.");
+});
