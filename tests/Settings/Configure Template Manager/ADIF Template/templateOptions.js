f210f3a90093cc0b413c627199683e27103eeecb
Samiha Hossain <samiha.hossain@selisegroup.com>
2026-03-15 16:16:00 +0600

    Feature/settings automation
    PR: #36

---
+++ tests/Settings/Configure Template Manager/ADIF Template/templateOptions.js
@@ -0,0 +1,8 @@
+export const templateOptions = [
+  "ADIF Estándar 1435 - Generales",
+  "ADIF Estándar 1435 - Tipo de línea A",
+  "ADIF Estándar 1435 - Tipo de línea B",
+  "ADIF Estándar 1435 - Tipo de línea C",
+  "ADIF Estándar 1435 - Tipo de línea D",
+  "ADIF Estándar 1435 - Tipo de línea E",
+];
