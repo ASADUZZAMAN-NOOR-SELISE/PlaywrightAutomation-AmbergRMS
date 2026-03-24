3314ec64169d617974532728544313cfae5fb162
Samiha Hossain <samiha.hossain@selisegroup.com>
2026-03-24 14:16:34 +0600

    feat: general info fields verification

---
+++ tests/Project Configuration/General Information & Parameters/gip.spec.js
@@ -0,0 +1,34 @@
+import { test, expect } from "@playwright/test";
+import path from "path";
+import { LoginPage } from "../../../Utils/loginPage";
+import GeneralInformationParametersPage from "./gip.page";
+
+let webContext;
+
+test.beforeAll("Navigated to dashboard", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const loginPage = new LoginPage(page);
+
+  await loginPage.goto();
+  await page.locator(".MuiGrid-root").nth(1).isVisible();
+  await loginPage.verifyInitialState();
+  await loginPage.login();
+  await loginPage.logoutVisible();
+
+  await context.storageState({ path: "state.json" });
+  webContext = await browser.newContext({ storageState: "state.json" });
+});
+
+test("General Information & Parameters validation", async () => {
+  const page = await webContext.newPage();
+
+  const loginPage = new LoginPage(page);
+  const gipPage = new GeneralInformationParametersPage(page);
+
+  await loginPage.goto();
+  await gipPage.navigateToGIP();
+  await gipPage.verifyGIPInformation();
+
+  console.log("General Information & Parameters tests passed successfully.");
+});
