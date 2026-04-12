cccff16b2b9d10c2ddd0ed76542b353ced92edf5
Samiha Hossain <samiha.hossain@selisegroup.com>
2026-04-12 17:14:56 +0600

    feat: gauge change validations added

---
+++ tests/Project Configuration/Gauge Change/gaugeChange.spec.js
@@ -0,0 +1,50 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../../Utils/loginPage";
+import { Common } from "../../../Utils/common";
+import GauageChangePage from "./gaugeChange.page";
+import { data } from "../../../Utils/Data/Information";
+
+let webContext;
+
+test.beforeAll("Navigated to dashboard", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const loginPage = new LoginPage(page);
+
+  await loginPage.goto();
+  await page.locator(".MuiGrid-root").nth(1).isVisible();
+  await loginPage.verifyInitialState();
+  await loginPage.login();
+  await loginPage.logoutVisible();
+
+  await context.storageState({ path: "state.json" });
+  webContext = await browser.newContext({ storageState: "state.json" });
+});
+
+test.beforeEach("Create new project", async () => {
+  const page = await webContext.newPage();
+  const loginPage = new LoginPage(page);
+  const common = new Common(page);
+
+  await loginPage.goto();
+  await common.clickNewProject();
+  await common.generalInformation({
+    name: `${data.templateName.en13848}-gaugechange`,
+    ...data.project,
+  });
+  await common.submitProject();
+  await expect(common.newProjectButton).toBeVisible();
+});
+
+test("Gauge Change validation", async () => {
+  const page = await webContext.newPage();
+
+  const loginPage = new LoginPage(page);
+  const gaugeChangePage = new GauageChangePage(page);
+
+  await loginPage.goto();
+  await gaugeChangePage.navigateToGaugeChange();
+  await gaugeChangePage.editGaugeChange();
+
+  console.log("Gauge Change tests passed successfully.");
+});
