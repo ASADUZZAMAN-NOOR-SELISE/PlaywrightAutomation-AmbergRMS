3c03405150d2570d8e4c5e31613989537d0a4d64
ASADUZZAMAN-NOOR-SELISE <asaduzzaman.noor@selisegroup.com>
2026-04-27 17:55:08 +0600

    Chart inspection (#65)
    * change time git action run 12AM every day once
    
    * update yml file
    
    * update config file

--- playwright.config.js
+++ playwright.config.js
@@ -29,17 +29,17 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html', { open: 'never' }],
-    ['allure-playwright']
-  ],
+  // reporter: [
+  //   ['html', { open: 'never' }],
+  //   ['allure-playwright']
+  // ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://dev-amberg.seliselocal.com/login?go=/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     //video: "only-on-failure",
   },
