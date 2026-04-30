178b41f8f12d2f696e7c80cc23f59775c64460f7
ASADUZZAMAN-NOOR-SELISE <asaduzzaman.noor@selisegroup.com>
2026-04-30 11:32:09 +0600

    chart filter , and zoom page

--- tests/Inspection/chart.spec.js
+++ tests/Inspection/chart.spec.js
@@ -1,139 +1,41 @@
-const { test, expect } = require('@playwright/test');
-import { JobPage } from '../ProjectTree/Job/job.page';
-import { LoginPage } from '../../Utils/loginPage';
-import { Common } from '../../Utils/common';
-import { ProjectTreePage } from '../ProjectTree/projectTree.page';
-//import path from 'path';
-import { loginForAll } from '../Logedin/loginForAll.page';
-import { ChartPage } from './chart.page';
+import { test, expect } from '@playwright/test';
 
-let webContext;
-
-// // function getUniqueProjectName(prefix = 'Line') {
-// //   return `${prefix}-${Date.now()}`;
-// // }
-
-test.beforeAll("Navigated to dashboard", async ({ browser }) => {
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  const loginForAllPage = new loginForAll(page);
-
-  await loginForAllPage.loginforall();
-  await context.storageState({ path: "state.json" });
-  webContext = await browser.newContext({ storageState: "state.json" });
-});
-
-
-// test('Import measurment from source folder ', async ({}) => {
-
-//   const page = await webContext.newPage();
-  
-//   const loginPage = new LoginPage(page);
-//   const common = new Common(page);
-//   const job = new JobPage(page);
-//   const tree = new ProjectTreePage(page);
-//   const projectName = getUniqueProjectName();
-
-//   const filePath = 'tests/Project Tree/Job/Measurment/09 Abs S_2023-10-11_112745.rmsmeas';
-  
-//   await loginPage.goto();
-//   await common.clickNewProject();
-//   await common.setProjectName(projectName);
-//   await common.submitProject();
-//   await common.searchProject(projectName);
-//   await expect(page.getByLabel(projectName).first()).toBeVisible();
-//   await common.enterIntoProject(projectName);
-//   await expect(page.getByRole('heading', { name: projectName })).toBeVisible();
-//   await tree.addLine("Line section 1");
-//   await tree.submitLineSectionBtn.isVisible();
-//   await tree.submitLineSectionBtn.click();
-//   await expect(page.getByRole('alert').first()).toContainText('Line section created successfully');
-
-//   // add track 
-//   await page.getByTestId('ChevronRightIcon').click();
-//   await expect(page.getByRole('button', { name: 'AddTrackButton' })).toBeVisible();
-//   await page.getByRole('button', { name: 'AddTrackButton' }).click();
-//   await expect(page.getByRole('button', { name: 'Custom Submit Button' })).toBeVisible();
-//   await expect(page.getByRole('heading', { name: 'Add Track' })).toBeVisible();
-//   await page.getByRole('button', { name: 'Custom Submit Button' }).click();
-
-//   // add track 
-//   await page.getByRole('textbox', { name: 'NameInput' }).click();
-//   await page.getByRole('textbox', { name: 'NameInput' }).fill('Track 1');
-//   await page.getByRole('textbox', { name: 'Number' }).click();
-//   await page.getByRole('textbox', { name: 'Number' }).fill('123456789');
-//   await page.getByRole('textbox', { name: 'Comment' }).click();
-//   await page.getByRole('textbox', { name: 'Comment' }).fill('track comment ');
-  
-//   await page.getByRole('spinbutton', { name: 'Start Localization [m]' }).fill('100');
-//   await page.getByRole('spinbutton', { name: 'End Localization [m]' }).fill('2000');
-//   await page.getByRole('button', { name: 'Custom Submit Button' }).click();
-//   await expect(page.getByTestId('line-section-tree-testid-child-0').getByText('Track')).toBeVisible();
-//   await page.getByTestId('line-section-tree-testid-child-0').getByText('Track').click();
-
-//   // add design
-//   await expect(page.locator(".project-tree-design")).toBeVisible();
-//   await page.locator(".project-tree-design").getByText("Add Design").click();
-//   await expect(page.getByTestId("add-design-section-drawer-test-id")).toBeVisible();
-//   await page.getByRole('textbox', { name: 'name' }).fill('Design 1');
-//   await page.getByTestId("CalendarIcon").click();
-//   await page.getByTestId("ArrowRightIcon").click();
-//   await page.locator("div[role='row'] button").filter({ hasText: '15' }).first().click();
-//   await page.getByRole('textbox', { name: 'Comment' }).fill('Design 1 comment');
-//   await page.getByRole('button', { name: 'Custom Submit Button' }).click();
-//   await expect(page.getByTestId('design-tree-testid')).toBeVisible();
-
-//   // add job
-//   await expect(page.locator(".project-tree-job")).toBeVisible();
-//   await job.clickAddJob();
-//   // fill form
-//   await job.fillName("Job 1");
-//   await job.fillComment("Job 1 comment");
-//   // submit
-//   await job.submitJob();
-  
-//   // import measurment 
-//   await page.getByTestId('MoreVertIcon').click();
-//   await expect(page.getByText('Import from AmbergRail')).toBeVisible();
-//   await page.getByText('Import from AmbergRail').click();
-
-//   // Ensure upload icon is ready
-//   const uploadBtn = page.getByAltText('upload icon');
-//   await expect(uploadBtn).toBeEnabled();
-
-//   // Start listener BEFORE click
-//   const [fileChooser] = await Promise.all([
-//     page.waitForEvent('filechooser'),
-//     uploadBtn.click()
-//   ]);
-
-//   await fileChooser.setFiles(path.resolve(filePath));
-//   await expect(page.getByText("This is a new measurement")).toBeVisible();
-//   await expect(await page.getByRole('button', { name: 'Custom Submit Button' })).toBeVisible();
-
-//   await page.getByRole('button', { name: 'Custom Submit Button' }).click();
-//   await expect(page.getByRole("alert").last()).toContainText("Import of the 09 Abs S_2023-10-11_11274… measurement successful. Please reload the views")
-//   await page.reload();
-//   await expect(page.locator(".project-tree-job li")).toBeVisible();
-
-//   await page.locator(".project-tree-job li").click();
-//   await expect(page.getByText('2023-10-11 15:27:45', { exact: true })).toBeVisible();
-//   await page.getByText('2023-10-11 15:27:45', { exact: true }).click();
-//   await expect(page.getByTestId("custom-side-bar").getByText("Measurement")).toBeVisible();
-
-// });
-
-
-test('Chart Filter', async () => {
-  const page = await webContext.newPage();
-  const loginPage = new LoginPage(page);
-  const common = new Common(page);
-  const tree = new ProjectTreePage(page);
-  const projectName = "Auto Inspection";
-
-  await loginPage.goto();
-  await common.searchProject(projectName);
-  await expect(page.getByLabel(projectName).first()).toBeVisible();
-  await common.enterIntoProject(projectName);
- 
+test('test', async ({ page }) => {
+  await page.goto('https://dev-amberg.seliselocal.com/login?go=/projects');
+  await page.getByRole('button', { name: 'Login submit button' }).click();
+  await page.getByRole('textbox', { name: 'Search by Project Name' }).click();
+  await page.getByRole('textbox', { name: 'Search by Project Name' }).pressSequentially('Auto Inspection');
+  await expect(page.getByLabel('#Auto Inspection')).toBeVisible();
+  await page.getByLabel('#Auto Inspection').click();
+  await page.getByTestId('line-section-tree-testid').getByText('line').click();
+  await page.getByTestId('line-section-tree-testid-child-0').getByText('Track').click();
+  await page.getByTestId('job-tree-testid').getByText('job').click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByTestId('job-tree-testid-child-0').getByRole('button').filter({ hasText: /^$/ }).click();
+  const page1 = await page1Promise;
+  await expect(page1.getByRole('heading')).toContainText('2024-02-23 18:00:02');
+  await page1.getByRole('button', { name: 'filter-button' }).click();
+  await expect(page1.getByTestId('chart-filter-section-drawer-test-id').getByRole('heading')).toContainText('Filter for Chart');
+  await page1.getByLabel('select-all-parameters').getByRole('checkbox').check();
+  await page1.getByLabel('select-all-parameters').getByRole('checkbox').uncheck();
+  await expect(page1.getByRole('button', { name: 'Custom Submit Button' })).toBeVisible();
+  await page1.getByRole('button', { name: 'Custom Submit Button' }).click();
+  await expect(page1.getByLabel('chart-error-text')).toContainText('No chart data found');
+  await page1.getByRole('button', { name: 'filter-button' }).click();
+  await page1.getByRole('checkbox').nth(1).check();
+  await page1.getByRole('button', { name: 'Custom Submit Button' }).click();
+  await expect(page1.getByTestId('canvasjs-container')).toContainText('Gauge Defect - Nominal Gauge to Peak [mm]');
+  await page1.getByRole('button', { name: 'filter-button' }).click();
+  await page1.getByRole('checkbox').nth(1).uncheck();
+  await page1.getByRole('checkbox').nth(2).check();
+  await page1.getByRole('button', { name: 'Custom Submit Button' }).click();
+  await expect(page1.getByTestId('canvasjs-container')).toContainText('Gauge Defect - Mean to Peak [mm]');
+  await page1.getByRole('button', { name: 'full-screen-button' }).click();
+  await expect(page1.locator('div').filter({ hasText: 'Charts20000.06Sample' }).nth(2)).toBeVisible();
+  await expect(page1.getByText('Charts').nth(1)).toBeVisible();
+  await expect(page1.getByRole('button', { name: 'new-tab-button' })).toBeVisible();
+  await expect(page1.getByRole('button', { name: 'full-screen-button' })).toBeVisible();
+  await expect(page1.getByRole('button', { name: 'next-button' })).toBeVisible();
+  await expect(page1.getByRole('button', { name: 'prev-button' })).toBeVisible();
+  await expect(page1.getByRole('button', { name: 'reload-button' })).toBeVisible();
 });
