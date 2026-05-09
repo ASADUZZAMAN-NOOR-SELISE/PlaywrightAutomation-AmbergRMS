60fafb9eccc6df1e85220ce54a82ab2369286975
Samiha Hossain <samiha.hossain@selisegroup.com>
2026-05-10 00:53:20 +0600

    feat: twist page test cases updated

--- tests/Project Configuration/Twist/twist.spec.js
+++ tests/Project Configuration/Twist/twist.spec.js
@@ -8,50 +8,51 @@ import TwistPage from "./twist.page.js";
 let webContext;
 
 test.beforeAll("Navigated to dashboard", async ({ browser }) => {
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    const loginPage = new LoginPage(page);
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const loginPage = new LoginPage(page);
 
-    await loginPage.goto();
-    await page.locator(".MuiGrid-root").nth(1).isVisible();
-    await loginPage.verifyInitialState();
-    await loginPage.login();
-    await loginPage.logoutVisible();
+  await loginPage.goto();
+  await page.locator(".MuiGrid-root").nth(1).isVisible();
+  await loginPage.verifyInitialState();
+  await loginPage.login();
+  await loginPage.logoutVisible();
 
-    await context.storageState({ path: "state.json" });
-    webContext = await browser.newContext({ storageState: "state.json" });
+  await context.storageState({ path: "state.json" });
+  webContext = await browser.newContext({ storageState: "state.json" });
 });
 
 test.beforeEach("Create new project", async () => {
-    const page = await webContext.newPage();
-    const loginPage = new LoginPage(page);
-    const common = new Common(page);
-    const editUnit = new EditUnitPage(page);
+  const page = await webContext.newPage();
+  const loginPage = new LoginPage(page);
+  const common = new Common(page);
+  const editUnit = new EditUnitPage(page);
 
-    await loginPage.goto();
-    await common.clickNewProject();
-    await common.generalInformation({
-        name: `${data.templateName.en13848}-twist`,
-        ...data.project,
-    });
-    await common.fillServiceProviderInfo(data.serviceProviderData);
-    await common.submitProject();
-    await expect(common.newProjectButton).toBeVisible();
+  await loginPage.goto();
+  await common.clickNewProject();
+  await common.generalInformation({
+    name: `${data.templateName.en13848}-twist`,
+    ...data.project,
+  });
+  await common.fillServiceProviderInfo(data.serviceProviderData);
+  await common.submitProject();
+  await expect(common.newProjectButton).toBeVisible();
 
-    // Set unit to meter for the project
-    await editUnit.setUnitMeter();
+  // Set unit to meter for the project
+  await editUnit.setUnitMeter();
 });
 
 test("Twist validation", async () => {
-    const page = await webContext.newPage();
+  const page = await webContext.newPage();
 
-    const loginPage = new LoginPage(page);
-    const twistPage = new TwistPage(page);
+  const loginPage = new LoginPage(page);
+  const twistPage = new TwistPage(page);
 
-    await loginPage.goto();
-    await twistPage.navigateToTwist();
-    await twistPage.verifyBaseValue();
-    await twistPage.addNewTwistBase();
+  await loginPage.goto();
+  await twistPage.navigateToTwist();
+  await twistPage.verifyBaseValue();
+  await twistPage.addNewTwistBase();
+  await twistPage.autoCalculateTwistLimits();
 
-    console.log("Twist tests passed successfully.");
+  console.log("Twist tests passed successfully.");
 });
