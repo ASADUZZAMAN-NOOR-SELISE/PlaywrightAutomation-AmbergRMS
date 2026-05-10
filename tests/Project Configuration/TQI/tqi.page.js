798c014048ad0723e94f8611de4016bced6c879d
Samiha Hossain <samiha.hossain@selisegroup.com>
2026-05-10 15:28:26 +0600

    feat: TQI test cases added

---
+++ tests/Project Configuration/TQI/tqi.page.js
@@ -0,0 +1,141 @@
+import { expect } from "@playwright/test";
+import { data } from "../../../Utils/Data/Information.js";
+
+const projectName = `${data.templateName.en13848}-tqi`;
+
+const expectedLimits = [
+  { lower: "-8.0", upper: "8.0" },
+  { lower: "-9.0", upper: "9.0" },
+  { lower: "-10.0", upper: "10.0" },
+];
+
+class TQIPage {
+  constructor(page) {
+    this.page = page;
+    this.projectsHeading = page.getByRole("heading", { name: "Projects" });
+    this.searchBox = page.getByRole("textbox", {
+      name: "Search by Project Name",
+    });
+    this.projectName = page.getByLabel(projectName);
+    this.projectConfig = page.getByText("Project Configuration");
+    this.editConfigBtn = page.getByRole("button", {
+      name: "Edit Configuration",
+    });
+    this.submitBtn = page.getByRole("button", {
+      name: "Custom Submit Button",
+    });
+    this.alert = page.getByRole("alert");
+    this.settingsButton = page.locator("button[aria-label='Settings']");
+    this.tqiBtn = page.getByRole("button", {
+      name: "Track Quality Index",
+    });
+    this.lengthInput = page.locator('[name="Tqi.SegmentSize"]');
+    this.lengthError = page.getByText("Value is required", { exact: true });
+    this.lengthRangeError = page.getByText(
+      "Value must be equal or greater than 0.10 m",
+      { exact: true },
+    );
+    this.calculationMethodDropdown = page.getByTitle("TUG TQI", {
+      exact: true,
+    });
+    this.isolatedDefectsOption = page.getByText("Isolated Defects");
+    this.alertLimitRadio = page.getByRole("radio", { name: "AlertLimit" });
+    this.tqiCalculationMethodText = page.getByText(
+      "TQI Calculation Method:Isolated Defects (After EN13848-6)",
+    );
+    this.severityLevelAlertText = page.getByText("Severity Level:Alert");
+    this.severityLevelInterventionText = page.getByText(
+      "Severity Level:Intervention",
+    );
+    this.severityLevelImmediateActionText = page.getByText(
+      "Severity Level:Immediate Action",
+    );
+    this.interventionLimitRadio = page.getByRole("radio", {
+      name: "InterventionLimit",
+    });
+    this.immediateActionLimitRadio = page.getByRole("radio", {
+      name: "ImmediateactionLimit",
+    });
+    this.comoboxIsolatedDefects = page.getByRole("combobox", {
+      name: "Isolated Defects",
+    });
+    // this.fiveParameterTrackOption = page.getByText("Five-Parameter Track");
+    this.fiveParameterTrackOption = page.locator(
+      ':text("Five-Parameter Track Defectiveness (w5)")',
+    );
+    this.fiveParameterText = page.getByText(
+      "TQI Calculation Method:Five-Parameter Track",
+    );
+  }
+
+  async navigateToTQI() {
+    await expect(this.projectsHeading).toHaveText("Projects");
+    await this.searchBox.click();
+    await this.searchBox.fill(projectName);
+    await this.projectName.first().click();
+    await this.projectConfig.click();
+    await this.editConfigBtn.click();
+    await this.tqiBtn.click();
+  }
+
+  async verifyMandatoryFieldValidation() {
+    await this.lengthInput.fill("");
+    await this.submitBtn.click();
+    await expect(this.lengthError).toBeVisible();
+    await this.lengthInput.fill("0.01");
+    await this.submitBtn.click();
+    await expect(this.lengthError).not.toBeVisible();
+    await expect(this.lengthRangeError).toBeVisible();
+    await this.lengthInput.fill("100");
+    await this.submitBtn.click();
+    await expect(this.lengthRangeError).not.toBeVisible();
+  }
+
+  async tqiCalculationMethod() {
+    await this.editConfigBtn.click();
+    await this.calculationMethodDropdown.click();
+    await this.isolatedDefectsOption.click();
+    await this.alertLimitRadio.check();
+    await this.submitBtn.click();
+    await expect(this.tqiCalculationMethodText).toBeVisible();
+    await expect(this.severityLevelAlertText).toBeVisible();
+    await this.editConfigBtn.click();
+    await this.interventionLimitRadio.check();
+    await this.submitBtn.click();
+    await expect(this.severityLevelInterventionText).toBeVisible();
+    await this.editConfigBtn.click();
+    await this.immediateActionLimitRadio.check();
+    await this.submitBtn.click();
+    await expect(this.severityLevelImmediateActionText).toBeVisible();
+    await this.editConfigBtn.click();
+    await this.comoboxIsolatedDefects.click();
+    await this.fiveParameterTrackOption.click();
+    await this.alertLimitRadio.click();
+    await this.submitBtn.click();
+    await expect(this.fiveParameterText).toBeVisible();
+    await expect(this.severityLevelAlertText).toBeVisible();
+    await this.editConfigBtn.click();
+    await this.interventionLimitRadio.check();
+    await this.submitBtn.click();
+    await expect(this.severityLevelInterventionText).toBeVisible();
+    await this.editConfigBtn.click();
+    await this.immediateActionLimitRadio.check();
+    await this.submitBtn.click();
+    await expect(this.severityLevelImmediateActionText).toBeVisible();
+    // await this.editConfigBtn.click();
+    // await this.page
+    //   .getByRole("combobox", { name: "Five-Parameter Track" })
+    //   .click();
+    // await this.page.getByText("Track Roughness Index (Amtrak)").click();
+    // await this.submitBtn.waitFor({ state: "visible" });
+    // await this.submitBtn.click();
+    // await expect(this.page.getByRole("alert")).toBeVisible();
+    // await expect(
+    //   this.page.getByText(
+    //     "TQI Calculation Method: Track Roughness Index (Amtrak)",
+    //   ),
+    // ).toBeVisible();
+  }
+}
+
+export default TQIPage;
---
+++ tests/Project Configuration/TQI/tqi.spec.js
@@ -0,0 +1,59 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../../Utils/loginPage";
+import EditUnitPage from "../Edit Unit Settings/editUnit.page";
+import { Common } from "../../../Utils/common";
+import { data } from "../../../Utils/Data/Information.js";
+import TQIPage from "./tqi.page.js";
+
+let webContext;
+
+test.beforeAll("Navigated to dashboard", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const loginPage = new LoginPage(page);
+
+  await loginPage.goto();
+  //   await page.locator(".MuiGrid-root").nth(1).isVisible();
+  await expect(page.getByTestId("login-image")).toBeVisible();
+  await loginPage.verifyInitialState();
+  await loginPage.login();
+  await loginPage.logoutVisible();
+
+  await context.storageState({ path: "state.json" });
+  webContext = await browser.newContext({ storageState: "state.json" });
+});
+
+test.beforeEach("Create new project", async () => {
+  const page = await webContext.newPage();
+  const loginPage = new LoginPage(page);
+  const common = new Common(page);
+  const editUnit = new EditUnitPage(page);
+
+  await loginPage.goto();
+  await common.clickNewProject();
+  await common.generalInformation({
+    name: `${data.templateName.en13848}-tqi`,
+    ...data.project,
+  });
+  await common.fillServiceProviderInfo(data.serviceProviderData);
+  await common.submitProject();
+  await expect(common.newProjectButton).toBeVisible();
+
+  // Set unit to meter for the project
+  await editUnit.setUnitMeter();
+});
+
+test("TQI validation", async () => {
+  const page = await webContext.newPage();
+
+  const loginPage = new LoginPage(page);
+  const tqiPage = new TQIPage(page);
+  await loginPage.goto();
+  await tqiPage.navigateToTQI();
+  await tqiPage.verifyMandatoryFieldValidation();
+  await tqiPage.tqiCalculationMethod();
+
+  //   await tqiPage.addChordLength();
+
+  console.log("TQI tests passed successfully.");
+});
