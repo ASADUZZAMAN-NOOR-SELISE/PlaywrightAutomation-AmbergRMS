789ea5d256d1116fc374863933de21364237454e
ASADUZZAMAN-NOOR-SELISE <asaduzzaman.noor@selisegroup.com>
2026-06-09 14:42:18 +0600

    update project create

--- tests/CreateProject/ADIF/adifMetricProject.js
+++ tests/CreateProject/ADIF/adifMetricProject.js
@@ -5,8 +5,8 @@ export class AdifMetricProject {
     this.page = page;
     this.configurationTemplateDropdown = page.getByRole('combobox', { name: 'Select configuration template' });
     this.templateOptionDropdown = page.getByRole('combobox', { name: 'Select Template Option' });
-    this.metricDefaultOption = page.getByRole('option', { name: 'ADIF Métrico 1000 - Default' });
-    this.metricGeneralOption = page.getByRole('option', { name: 'ADIF Métrico 1000 - Generales' });
+    this.metricDefaultOption = page.getByRole('option', { name: 'ADIF Métrico 1000 - Generales' });
+    this.metricGeneralOption = page.getByRole('option', { name: 'ADIF Métrico 1000 - No Generales' });
     this.templateOptionDropdownMetric = page.getByRole('combobox', { name: 'Select Template Option ADIF M' });
     this.listbox = page.getByRole('listbox');
   }
@@ -15,14 +15,14 @@ export class AdifMetricProject {
     await this.configurationTemplateDropdown.click();
     await this.page.getByRole('option', { name: configTemplateValue }).click();
     await this.templateOptionDropdown.click();
-    await expect(this.listbox).toContainText('ADIF Métrico 1000 - Default');
     await expect(this.listbox).toContainText('ADIF Métrico 1000 - Generales');
+    await expect(this.listbox).toContainText('ADIF Métrico 1000 - No Generales');
     await expect(this.metricDefaultOption).toBeVisible();
     await this.metricDefaultOption.click();
     if (await this.templateOptionDropdownMetric.count()) {
       await this.templateOptionDropdownMetric.click();
-      await expect(this.listbox).toContainText('ADIF Métrico 1000 - Default');
       await expect(this.listbox).toContainText('ADIF Métrico 1000 - Generales');
+      await expect(this.listbox).toContainText('ADIF Métrico 1000 - No Generales');
       await this.metricDefaultOption.click();
     }
   }
