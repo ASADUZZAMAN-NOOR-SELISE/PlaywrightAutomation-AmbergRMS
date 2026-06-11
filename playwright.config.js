4fb096b6714be4e459651ab7f9686dc5eb82b2f5
ASADUZZAMAN-NOOR-SELISE <asaduzzaman.noor@selisegroup.com>
2026-06-11 15:36:31 +0600

    report add alure and junit.xml

--- playwright.config.js
+++ playwright.config.js
@@ -31,6 +31,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
+    ['allure-playwright'],
     ['junit', { outputFile: 'test-results/junit.xml' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
